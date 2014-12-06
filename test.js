0b3ddcfb17f53327a8b39faaa9589ed1ba0bbc98
Hiroto Kitamura <kirikonknow@gmail.com>
2014-12-06 19:24:46 +0900

    koa-router使ってみるテスト

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
-var koa = require('koa');
-var app = koa();
+var koa = require('koa'),
+    router = require('koa-router'),
+    app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
+app.use(router(app));
+
+app.get('/test', function *(next) {
+    this.body = "list page";
+});
+
+app.get('/test/:id', function *(next) {
+    this.body = "id : " + this.params.id;
+});
+
+app.post('/test', function *(next) {
+    this.body = "regist";
+});
+
+app.put('/test/:id', function *(next) {
+    this.body = "update id : " + this.params.id;
+});
+
+app.del('/test/:id', function *(next) {
+    this.body = 'delete id : ' + this.params.id;
 });
 
 app.listen(process.env.PORT || 5000);
